285bc58c58b7d014ce3a14173c60d2fd5fe12b20
Halat <halatnaby@hotmail.com>
2015-05-05 13:36:18 +0000

    commit 4

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -3,13 +3,13 @@ import sys
 file = open("prime.dat" , "w")
 
 N = int(sys.argv[1])
-count = 0
 List = list(range(0, N+1))
+count = 0
 start = time.clock()
 for i in range(2, N+1):
     if i in List:
         file.write(str(i)+"\n")
-        count = count +1
+        count = count + 1
         for j in range (2, int(N/i)+1):
             List[i*j] = 0
             
