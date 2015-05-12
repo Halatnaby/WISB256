81acd6b26cdec9082da4d3a5ef0b4f86cb553022
Halat <halatnaby@hotmail.com>
2015-05-12 07:11:45 +0000

    Improved Sieve

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,13 +1,13 @@
 import time
 import sys
-file = open("prime.dat" , "w")
+file = open(sys.argv[2] , "w")
 
 N = int(sys.argv[1])
 List = list(range(0, N+1))
 count = 0
 start = time.clock()
 for i in range(2, N+1):
-    if i in List:
+    if List[i] !=0:
         file.write(str(i)+"\n")
         count = count + 1
         for j in range (2, int(N/i)+1):
